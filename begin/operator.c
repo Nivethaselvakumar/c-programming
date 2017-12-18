bc1b0d635ffc42c8da08be8e9aff80c10d92e2bf
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-19 00:16:46 +0530

    Update operator.c

--- begin/operator.c
+++ begin/operator.c
@@ -4,11 +4,11 @@ int main(void)
 {
 int a,b;
 scanf("%d %d",&a,&b);
-printf("%d\n",a+b);
-printf("%d\n",a-b);
-printf("%d\n",a*b);
-printf("%d\n",a/b);
-printf("%d\n",a%b);
+printf("%d,",a+b);
+printf("%d,",a-b);
+printf("%d,",a*b);
+printf("%d,",a/b);
+printf("%d,",a%b);
 
 
 	return 0;
