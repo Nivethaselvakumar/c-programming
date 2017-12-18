3940ced65b9c49d670562a1dc86f367f2b7c4d3b
Nivethaselvakumar <34640693+Nivethaselvakumar@users.noreply.github.com>
2017-12-18 14:24:32 +0530

    Create data.c

---
+++ begin/data.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+char a[50]="Sachin Ramesh Tendulkar";
+char b[20]="Little Master";
+int p=18426;
+float q=44.83;
+int r=463;
+printf("%s\n",a);
+printf("%s\n",b);
+printf("%d\n",p);
+printf("%.2f\n",q);
+printf("%d\n",r);
+return 0;
+}
